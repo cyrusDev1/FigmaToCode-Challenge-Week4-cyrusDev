91d19a7d7e95fb9f751125d4327a2fdd74e2c9ab
cyrusDev1 <cyrushoungue1240@gmail.com>
2024-02-18 16:41:33 +0100

    adding title

--- components/Menu/header.vue
+++ components/Menu/header.vue
@@ -1,5 +1,8 @@
 <template>
     <header class="mx-5 2xl:ml-[102px] 2xl:mr-28 py-4" id="home">
+        <Head>
+            <Title>CyrusDev | Developper</Title>
+        </Head>
         <NavBar class="hidden lg:block"></NavBar>
         <MobileNavBar class="block lg:hidden"></MobileNavBar>
     </header>
